d455a117f7632bd1c829452fc954a599bc2bcd10
Farshad HN <sfarshadhn@outlook.com>
2021-09-03 14:36:02 +0430

    Gender Type and autofac dependencies are moved to core layed. Version Changed

--- Lookif.Common/Enums/GenderType.cs
+++ Lookif.Common/Enums/GenderType.cs
@@ -2,12 +2,5 @@
 
 namespace Lookif.Library.Common.Enums
 {
-    public enum GenderType
-    {
-        [Display(Name = "مرد")]
-        Male = 1,
 
-        [Display(Name = "زن")]
-        Female = 2
-    }
 }
