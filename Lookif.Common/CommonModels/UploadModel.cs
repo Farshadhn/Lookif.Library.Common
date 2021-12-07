b04ce8c8fa2219714ba221a60dd6bcb1be0b9e17
Farshad <sfarshadhn@outlook.com>
2021-12-07 13:44:58 +0330

    Upload

---
+++ Lookif.Common/CommonModels/UploadModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lookif.Library.Common.CommonModels
+{
+    public class UploadModel
+    {
+        public byte[] File{ get; set; }
+        public string FileName{ get; set; }
+    }
+}
--- Lookif.Common/Utilities/IOFunctions.cs
+++ Lookif.Common/Utilities/IOFunctions.cs
@@ -9,22 +9,33 @@ namespace Lookif.Library.Common.Utilities
 {
     public static class IOFunctions
     {
-        public static async Task SaveTo(byte[] fileData, string FileName, string Location = "temp")
+        /// <summary>
+        /// Save bytes to directory
+        /// </summary>
+        /// <param name="fileData"></param>
+        /// <param name="FileName"></param>
+        /// <param name="Location"></param>
+        /// <param name="asbAddress"></param>
+        /// <returns></returns>
+        public static async Task<string> SaveTo(byte[] fileData, string FileName, string Location = "temp",string absoluteAddress = "")
         {
-            string filePath = Environment.CurrentDirectory + $"/{Location}/";
+            string filePath =(string.IsNullOrEmpty(absoluteAddress)?Environment.CurrentDirectory: absoluteAddress) + $"/{Location}/";
             System.IO.FileInfo file = new System.IO.FileInfo(filePath);
             file.Directory.Create(); // If the directory already exists, this method does nothing.
-            await File.WriteAllBytesAsync($"{filePath}{DateTime.Now.ToString("yyyy-MM-dd")}-{FileName}", fileData);
-
+            var prefix = DateTime.Now.ToString("yyyy-MM-dd") + Guid.NewGuid().ToString().Substring(1,5);
+            await File.WriteAllBytesAsync($"{filePath}{prefix}-{FileName}", fileData);
+            return $"{prefix}-{FileName}";
         }
-        public static async Task SaveTo(string fileData, string FileName, string Location = "temp")
+        public static async Task<string> SaveTo(string fileData, string FileName, string Location = "temp", string absoluteAddress = "")
         {
             var fileInBytearr = Encoding.ASCII.GetBytes(fileData);
-            string filePath = Environment.CurrentDirectory + $"/{Location}/";
+            string filePath = (string.IsNullOrEmpty(absoluteAddress) ? Environment.CurrentDirectory : absoluteAddress) + $"/{Location}/";
+             
             System.IO.FileInfo file = new System.IO.FileInfo(filePath);
             file.Directory.Create(); // If the directory already exists, this method does nothing.
-            await File.WriteAllBytesAsync($"{filePath}{DateTime.Now.ToString("yyyy-MM-dd")}-{FileName}", fileInBytearr);
-
+            var prefix = DateTime.Now.ToString("yyyy-MM-dd") + Guid.NewGuid().ToString().Substring(1, 5);
+            await File.WriteAllBytesAsync($"{filePath}{prefix}-{FileName}", fileInBytearr);
+            return $"{prefix}-{FileName}";
         }
     }
 }
