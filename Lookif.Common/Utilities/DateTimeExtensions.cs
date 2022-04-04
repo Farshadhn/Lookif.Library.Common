4295901e4589f4178d34711bab5e4d2f34c296e0
sfarshadhn <sfarshadhn@outlook.com>
2022-04-04 18:04:39 +0430

    persian is now supported in DateTimeExtension for StartOfMonth and Year

--- Lookif.Common/Utilities/DateTimeExtensions.cs
+++ Lookif.Common/Utilities/DateTimeExtensions.cs
@@ -1,20 +1,25 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Lookif.Library.Common.Utilities;
 
 public static class DateTimeExtensions
 {
-    public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
+    public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Saturday)
     {
         int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
         return dt.AddDays(-1 * diff).Date;
     }
-    public static DateTime StartOfMonth(this DateTime dt)
+    public static DateTime StartOfMonth(this DateTime dt, bool persian = true)
     {
-        return new DateTime(dt.Year, dt.Month, 1);
+        var pNow = new PersianCalendar();
+        return (persian) ? dt.AddDays(-1 * pNow.GetDayOfMonth(dt) + 1) : new DateTime(dt.Year, dt.Month, 1);
     }
-    public static DateTime StartOfYear(this DateTime dt)
+
+    public static DateTime StartOfYear(this DateTime dt, bool persian = true)
     {
-        return new DateTime(dt.Year,1, 1);
+        var pNow = new PersianCalendar(); 
+        return (persian) ? dt.AddMonths(-1 * pNow.GetMonth(dt) + 1).StartOfMonth() : new DateTime(dt.Year, 1, 1); 
+         
     }
 }
