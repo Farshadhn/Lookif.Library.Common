8a9f392e1f42aecd8ef853abf598209a3e5fb103
sfarshadhn <sfarshadhn@outlook.com>
2022-04-04 11:07:16 +0430

    StartOfWeek,StartOfMonth,StartOfYear

---
+++ Lookif.Common/Utilities/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lookif.Library.Common.Utilities;
+
+public static class DateTimeExtensions
+{
+    public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
+    {
+        int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+        return dt.AddDays(-1 * diff).Date;
+    }
+    public static DateTime StartOfMonth(this DateTime dt)
+    {
+        return new DateTime(dt.Year, dt.Month, 1);
+    }
+    public static DateTime StartOfYear(this DateTime dt)
+    {
+        return new DateTime(dt.Year,1, 1);
+    }
+}
