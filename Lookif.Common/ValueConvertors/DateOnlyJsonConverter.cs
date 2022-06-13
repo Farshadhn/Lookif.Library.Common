aea0d80ab707aeabe9cb9e27bc547ecb943918c6
sfarshadhn <sfarshadhn@outlook.com>
2022-06-13 10:31:10 +0430

    DateOnlyJsonConverter is added to convert dateonly when using json

---
+++ Lookif.Common/ValueConvertors/DateOnlyJsonConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Lookif.Library.Common.ValueConvertors;
+
+public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+
+{
+	public override void Write(Utf8JsonWriter writer, DateOnly date, JsonSerializerOptions options)
+	{
+		writer.WriteStringValue(date.ToString());
+	}
+	public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		return DateOnly.FromDateTime(reader.GetDateTime());
+	}
+}
