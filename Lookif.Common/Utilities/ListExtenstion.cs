b7b940c65003d7c6fee8518042d3475200abc42c
Farshad <sfarshadhn@gmail.com>
2023-05-06 21:41:15 +0330

    NeverNull and NeverNullConcat=> V 2.2.0

--- Lookif.Common/Utilities/ListExtenstion.cs
+++ Lookif.Common/Utilities/ListExtenstion.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Lookif.Library.Common.Utilities
 {
@@ -7,6 +8,16 @@ namespace Lookif.Library.Common.Utilities
         public static List<T> GetClone<T>(this List<T> source)
         {
             return source.GetRange(0, source.Count);
+        }
+        public static IEnumerable<T> NeverNull<T>(this IEnumerable<T> value)
+        {
+            return value ?? Enumerable.Empty<T>();
+        }
+        public static IEnumerable<T> NeverNullConcat<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            first ??= Enumerable.Empty<T>();
+            second ??= Enumerable.Empty<T>();
+            return first.Concat(second);
         }
     }
 }
